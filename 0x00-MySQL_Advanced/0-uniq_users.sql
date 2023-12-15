998d53893cef8a63f44c10978c3d2e09edb413ed
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-15 00:53:48 +0000

    Added a new Mysql file

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,13 @@
+-- This script creates the 'users' table with the specified attributes
+
+-- Check if the table already exists
+IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
+
+    -- Create the 'users' table
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255)
+    );
+
+END IF;
