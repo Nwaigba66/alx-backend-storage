72f00b6a4ada6aad9a12b53a0e0a8323a1ccb7e8
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-19 12:57:41 -0800

    A Python function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Define a function that list documents in a mongodb collection
+"""
+
+
+def list_all(mongo_collection):
+    """List documents in a given collection
+
+    Arguments:
+    =========
+    mongo_collection - a mongodc collection
+
+    Returns - List of documents if any or empty list
+    """
+    documents = mongo_collection.find()
+    return list(documents)
+
+# Import necessary modules and connect to the MongoDB server
+from pymongo import MongoClient
+
+# Replace 'your_connection_string' with the actual connection string
