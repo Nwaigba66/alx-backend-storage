4dd63e60dd30dbadf1569a5198820c4dc94e7a25
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-20 19:12:18 +0000

    Updated codes and resolved errors

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,7 @@ import uuid
 from typing import Union
 
 types = [str, bytes, int, float]
-GetReturnType = Union[int, str, float, bytes, None]
+
 
 class Cache:
     def __init__(self):
@@ -27,7 +27,6 @@ class Cache:
         """Store the input data in redis using the random key
         """
         self._redis.set(key, data)
-        
         """Return the key
         """
         return key
