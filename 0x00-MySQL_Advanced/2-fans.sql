da34dfcc9b2d426a391f702cf773d78c227eea6e
Nwaigba66 <glorianwaigba@gmail.com>
2024-02-16 16:25:07 -0800

    Added a script Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
--- Ranks country origin of bands, ordered by the number of
+-- Ranked country origin of bands, ordered by the number of
 -- non-unique fans
 SELECT origin, COUNT(*) nb_fans FROM metal_bands
 GROUP BY origin ORDER BY nb_fans DESC;
