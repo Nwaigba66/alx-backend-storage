d6453c355b55e62713bbcd7d689d2e0bb0ba3a9a
Nwaigba66 <glorianwaigba@gmail.com>
2024-02-16 16:34:44 -0800

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,14 @@
+-- Import the provided table dump
+-- Assuming the table is named "metal_bands"
+
+-- Calculate the lifespan in years until 2022
 SELECT 
     band_name,
-    CASE 
-        WHEN lifespan = 'split -' THEN NULL
-        ELSE YEAR('2022-01-01') - SUBSTRING_INDEX(lifespan, ' - ', 1)
-    END AS lifespan
+    YEAR('2022-01-01') - SUBSTRING_INDEX(lifespan, ' - ', 1) AS lifespan
 FROM
     metal_bands
 WHERE
     main_style = 'Glam rock'
 ORDER BY
     lifespan DESC;
+
