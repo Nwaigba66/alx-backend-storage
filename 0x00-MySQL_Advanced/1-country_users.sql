3b0f6e37be2a6aaccd2d9b7c45e2bd816e251e25
Nwaigba66 <glorianwaigba@gmail.com>
2024-02-16 16:05:40 -0800

    Created SQL script that creates a table users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Creates table named user with attributes id, email
+-- name and coutry
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
